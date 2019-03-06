7685c272bde29914b1db30c8ad6276a7d5a52216
Key <keyla@moogah.com>
2019-03-06 16:12:01 -0300

    updated vitt app for wh report

---
+++ odoo-argentina-vitt/vitt_wihholding_tax_report/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'wihholding tax report',
+    'description': 'wihholding tax report',
+    'summary': 'wihholding tax report',
+    'author': 'Moogah',
+    'website': 'http://www.moogah.com',
+    'version': '10.0.1.0.1',
+    'license': 'Other proprietary',
+    'maintainer': 'Osvaldo Jorge Gentile',
+    'contributors': '',
+    'category': 'Localization',
+    'depends': ['vitt_sales_reports','vitt_payments_odoo_std'],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+}
