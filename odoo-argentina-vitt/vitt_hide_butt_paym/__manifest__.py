08289b45818231b8695f7c7b21e4b5f477ffb373
Key <keyla@moogah.com>
2019-05-03 16:27:10 -0300

    update vitt

---
+++ odoo-argentina-vitt/vitt_hide_butt_paym/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hide pyament button invoice",
+    'summary': """hide pyament button invoice""",
+    'description': """hide pyament button invoice""",
+    'author': "Moogah",
+    'website': "http://www.Moogah.com",
+    'category': 'Uncategorized',
+    'version': '10.0.1.0.0',
+    'depends': ['account_payment_group'],
+    'data': [
+        'views/views.xml',
+    ],
+    'demo': [],
+    'application': True,
+}
