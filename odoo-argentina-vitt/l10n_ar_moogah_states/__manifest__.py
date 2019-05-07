440d663f0a7501d36985791c098b586a7b445a90
Key <keyla@moogah.com>
2019-05-07 13:34:09 -0300

    added states

--- odoo-argentina-vitt/l10n_ar_moogah_states/__manifest__.py
+++ odoo-argentina-vitt/l10n_ar_moogah_states/__manifest__.py
@@ -3,7 +3,6 @@
     "name": "Provincias Argentinas con Cod. AFIP",
     'version': '10.0.1.2',
     'category': 'Localization/Argentina',
-    'sequence': 14,
     'author': 'Moogah',
     'license': 'AGPL-3',
     'summary': """App de actualizacion para cod. AFIP en Provincias""",
@@ -17,4 +16,3 @@
     'auto_install': False,
     'application': False,
 }
-#<div>Icons made by <a href="https://www.flaticon.com/authors/twitter" title="Twitter">Twitter</a> from <a href="https://www.flaticon.com/" 		    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
