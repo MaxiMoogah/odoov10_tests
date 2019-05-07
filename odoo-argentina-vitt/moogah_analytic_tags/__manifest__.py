c261a973f2f1dcf18d130e91fda250d167ac4b50
Key <keyla@moogah.com>
2019-05-07 13:31:28 -0300

    added states

--- odoo-argentina-vitt/moogah_analytic_tags/__manifest__.py
+++ odoo-argentina-vitt/moogah_analytic_tags/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Moogah - Installer for Analytic Tags apps", 
-    'version': '10.0.1.2',
+    'version': '10.0.1.1',
     'description': """
         Instala de forma automática todos los módulos que se necesitan para utilizar Etiquetas Analíticas en Compras (Cotizaciones, Ordenes y Facturas),
         Ventas (Cotizaciones, Ordenes y Facturas), suscripciones y Asientos Contables. No incluye el soporte para Bienes de Uso. 
