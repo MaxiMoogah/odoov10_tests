57e90d344e4b55fef2f3cc34daa9093f985b9532
Key <keyla@moogah.com>
2019-05-07 13:00:44 -0300

    removed link

--- odoo-argentina-vitt/l10n_ar_moogah_chart/__manifest__.py
+++ odoo-argentina-vitt/l10n_ar_moogah_chart/__manifest__.py
@@ -24,4 +24,3 @@ Plan contable argentino e impuestos de acuerdo a disposiciones vigentes
         'data/check_accounts_data.xml'
     ],
 }
-#<div>Icons made by <a href="https://www.flaticon.com/authors/twitter" title="Twitter">Twitter</a> from <a href="https://www.flaticon.com/" 		    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 		    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
