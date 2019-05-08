8072aff9d26f2f6fe8d32e1a221cd80ee61f324d
Key <keyla@moogah.com>
2019-05-08 15:39:56 -0300

    update invoice arg format

--- odoo-argentina-vitt/vitt_arg_einvoice_format/__manifest__.py
+++ odoo-argentina-vitt/vitt_arg_einvoice_format/__manifest__.py
@@ -7,11 +7,11 @@
 ##############################################################################
 {
     'name': 'VITT ARG EInvoice Format ',
-    'description': 'Adds Argentinian einvoice format',
-    'summary': '',
+    'description': 'Formato de impresión para Facturas Argentina',
+    'summary': 'Este app instala el formato para Impresión de Facturas según requerimientos de Argentina',
     'author': 'Business Analytics Consulting Group S.A. de C.V.',
     'website': 'http://www.bacgroup.net',
-    'version': '10.0.1.0.4',
+    'version': '10.0.1.0.5',
     'license': 'Other proprietary',
     'maintainer': 'Salvatore Josue Trimarchi Pinto',
     'contributors': '',
