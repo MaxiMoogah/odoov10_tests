4260b7baf79f9b8bf74ec7056fbd7966f257eb4d
Key <keyla@moogah.com>
2019-05-13 14:39:45 -0300

    updated percep report

--- odoo-argentina-vitt/vitt_percep_tax_report/__manifest__.py
+++ odoo-argentina-vitt/vitt_percep_tax_report/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Informe Imp. Percepciones - Facturas',
-    'version': '10.0.1.0.7',
+    'version': '10.0.1.0.6',
     'summary': 'Informes de Impuestos de Percepción en Facturas (Ventas y Compras)',
     'author': 'Moogah',
     'website': 'http://www.moogah.com',
