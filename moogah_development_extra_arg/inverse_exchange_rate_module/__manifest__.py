d01be8a7c76c9422f00e7155e67ccf167428d909
Key <keyla@moogah.com>
2019-05-14 12:15:19 -0300

    removed anticipate list menu

--- moogah_development_extra_arg/inverse_exchange_rate_module/__manifest__.py
+++ moogah_development_extra_arg/inverse_exchange_rate_module/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Inverse Exchange Rate",
-    "version": "10.0.1.0.16",
+    "version": "10.0.1.0.17",
     "depends": ['base', 'account', 'account_accountant'],
     "author": "BrowseInfo, Moogah",
     "description": """
