4efe61d52f1dbf31e66f66999c97ca604cca582d
Key <keyla@moogah.com>
2019-05-14 12:00:00 -0300

    customer outstanding report

---
+++ odoo-argentina-vitt/vitt_customer_outstanding_statement/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Customer/Supplier Outstanding Statement Extended",
+    'summary': """Customer/Supplier Outstanding Statement Extended""",
+    'description': """Customer/Supplier Outstanding Statement Extended""",
+    'author': "Moogah",
+    'website': "http://www.Moogah.com",
+    'category': 'Uncategorized',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'customer_outstanding_statement',
+        'account',
+    ],
+    'data': [
+        'views/views.xml',
+    ],
+    'demo': [],
+    'application': True,
+}
