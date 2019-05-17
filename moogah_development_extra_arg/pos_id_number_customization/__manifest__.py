696673932b0d10f661e144fa73c5970334f93f4e
Key <keyla@moogah.com>
2019-05-17 12:59:54 -0300

    changed version+icon+description

--- moogah_development_extra_arg/pos_id_number_customization/__manifest__.py
+++ moogah_development_extra_arg/pos_id_number_customization/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "POS Identification Number Customization",
-    'summary': """POS Identification Number Customization""",
+    'name': "POS Busqueda de Cliente por Id. Tributario",
+    'summary': """POS Busqueda de Cliente por Id. Tributario""",
     'description': """
     This module allows the search of customers by Main Identification Number.
     """,
     'author': "Moogah",
     'website': "www.moogah.com",
     'category': 'Point of Sale',
-    'version': '10.0.1.0',
+    'version': '10.0.1.1',
     'depends': ['l10n_ar_partner', 'point_of_sale'],
     'qweb': [
     ],
