0764abe89db724a9326f33389d8cd3cc96f9a55b
Key <keyla@moogah.com>
2019-05-20 15:34:10 -0300

    boton_mercadopago

---
+++ odoo-extra-arg/boton_mercadopago-master/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Button for MercadoPago',
+    'category': 'Account',
+    'version': '10.0.1.0.0',
+    'depends': [
+        'account',
+        'sale',
+    ],
+    'data': [
+        'views/invoice_view.xml',
+        'views/invoice_action_data.xml'
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
