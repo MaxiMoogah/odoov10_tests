fc6259fcf34b8125bcb22c955dc72da09b2f5f52
Key <keyla@moogah.com>
2019-05-20 13:39:38 -0300

    adding button

---
+++ clientes/boton_mercadopago-master/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Button for MercadoPago',
+    'category': 'Account',
+    'version': '0.1',
+    'depends': ['account','base','edi'],
+    'data': [
+	'invoice_view.xml',
+	'invoice_action_data.xml'
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
